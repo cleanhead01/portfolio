d9631a484041c4fc1c71d7d3dd594f70eb12ba2b
cleanhead01 <seokjh93@naver.com>
2020-08-27 21:45:45 +0900

    튜토리얼 페이지 추가

---
+++ js/index.js
@@ -0,0 +1,16 @@
+(function($){
+
+
+    // 로딩페이지-----------------------------------------
+    $(window).load(function(){
+        $('.waiting-box').delay(3800).fadeOut()
+    })
+    
+    // function btn(){
+    //     alert('연습을 실전처럼!<br> 게임시작을 누르세요!')
+    // }
+
+
+
+
+}) (jQuery)
