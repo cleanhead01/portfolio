c92236601feafa86bf4bec4ed247c24b80b36004
cleanhead01 <seokjh93@naver.com>
2020-08-29 23:42:40 +0900

    첫페이지 마무리 직전

--- js/index.js
+++ js/index.js
@@ -6,9 +6,26 @@
         $('.waiting-box').delay(3800).fadeOut()
     })
     
-    // function btn(){
-    //     alert('연습을 실전처럼!<br> 게임시작을 누르세요!')
-    // }
+    // 첫번째 미역 클릭하면 about me 오픈이벤트
+    $('.article1 .seaweed1 > a').on('click', function(){
+        $('.about-me').addClass('on')
+    })
+
+    $('.article1 .close').on('click', function(){
+        $('.about-me').removeClass('on')
+    })
+
+
+
+
+
+
+
+    // about me 페이지 클릭이벤트
+    $('.article1 .career .month').on('click', function(){
+        $(this).addClass('on')
+        .siblings().removeClass('on')
+    })
 
 
 
