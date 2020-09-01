d111e7b640b7679622ffc3d92c5a671f4d2f31b0
cleanhead01 <seokjh93@naver.com>
2020-09-01 22:21:46 +0900

    contact 페이지 추가

--- js/index.js
+++ js/index.js
@@ -6,26 +6,32 @@
         $('.waiting-box').delay(3800).fadeOut()
     })
     
-    // 첫번째 미역 클릭하면 about me 오픈이벤트
+    // 첫번째 미역 클릭하면 about me 오픈이벤트-----------------------------------------
     $('.article1 .seaweed1 > a').on('click', function(){
         $('.about-me').addClass('on')
     })
-
+    // about me 페이지 나가기 버튼
     $('.article1 .close').on('click', function(){
         $('.about-me').removeClass('on')
     })
-
-
-
-
-
-
-
     // about me 페이지 클릭이벤트
     $('.article1 .career .month').on('click', function(){
         $(this).addClass('on')
         .siblings().removeClass('on')
     })
+
+
+
+
+    // 네번째 미역 클릭하면 contact 오픈 이벤트------------------------------------------
+    $('.article3 .seaweed2 > a').on('click', function(){
+        $('.contact').addClass('on')
+    })
+    // contact 페이지 나가기 버튼
+    $('.article3 .close').on('click', function(){
+        $('.contact').removeClass('on')
+    })
+    
 
 
 
