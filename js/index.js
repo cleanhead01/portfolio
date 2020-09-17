34e6058674daaf85fd3bffc10c041b90f2c9a3d5
cleanhead01 <seokjh93@naver.com>
2020-09-17 21:17:16 +0900

    수정하려다 취소

--- js/index.js
+++ js/index.js
@@ -60,14 +60,6 @@
 
 
 
-
-
-
-
-
-
-
-
     // 첫번째 미역 클릭하면 about me 오픈이벤트-----------------------------------------
     $('.article1 .seaweed1 > a').on('click', function(){
         $('.about-me').addClass('on')
